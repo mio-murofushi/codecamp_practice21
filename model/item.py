fc735cc7348ce70adb0a8145edefd84cf2063165
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-07-09 11:47:33 +0900

    update: DB item

--- model/item.py
+++ model/item.py
@@ -9,8 +9,28 @@ class Item:
         社員番号
     employee_name : str
         社員の名前
+    employee_age : int
+        社員の年齢
+    gender : str
+        社員の性別 
+    photo_id : str
+        社員の写真id
+    adress : str
+        社員の住所
+    department_id : str
+        所属している部署No.
+    photo_name : str
+        社員の写真ファイルネーム
+    department_name : str
+        部署名
     """
-    def __init__(self, employee_id="", employee_name=""):
+    def __init__(self, employee_id="", employee_name="", employee_age="", gender="", photo_id="", adress="", department_id="", photo_name="",department_name=""):
         self.employee_id = employee_id
         self.employee_name = employee_name
-        
+        self.employee_age = employee_age
+        self.gender = gender
+        self.photo_id = photo_id
+        self.adress = adress
+        self.department_id = department_id
+        self.department_name = department_name
+        self.photo_name = photo_name
